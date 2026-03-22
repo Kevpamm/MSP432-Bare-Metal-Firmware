d5ae90c35ff2989d4e6ac8905e4aa731f340c9b4
Kevin Pham <khanpg2004@gmail.com>
2026-03-21 23:22:34 -0600

    Fix incorrect source and include paths in Makefile and sources.mk   - Source files now correctly referenced udner src/   - Include paths updated to include/common and include/msp432   - Linker script path corrected from ../msp432p401r.lds to msp432p401r.lds
    All paths are now relative to Cortex-M4/ where make is invoked.

--- Cortex-m4/sources.mk
+++ Cortex-m4/sources.mk
@@ -1,29 +1,37 @@
-#******************************************************************************
-# Copyright (C) 2017 by Alex Fosdick - University of Colorado
+#------------------------------------------------------------------------------
+# This file defines the source files and header files Makefile uses depending
+# on the target platform (HOST or MSP432)
 #
-# Redistribution, modification or use of this software in source or binary
-# forms is permitted as long as the files maintain this copyright. Users are 
-# permitted to modify this and use it to learn about the field of embedded
-# software. Alex Fosdick and the University of Colorado are not liable for any
-# misuse of this material. 
+# This file is included in the Makefile
 #
-#*****************************************************************************
+# Author: Khanh Pham (Kevin)
+# Date:   February 2026
+#
+# Sources:
+#      HOST     - Compiles main.c and memory.c only
+#      MSP432   - Additionally compiles the MSP432 startup, interrupt, and
+#                 system initialization files required for bare-metal boot
+#
+# Includes:
+#      HOST     - Common headers shared across both platforms
+#      MSP432   - Additionally includes MSP432 and CMSIS device headers
+#
+#------------------------------------------------------------------------------
 
-# Add your Source files to this variable
-SOURCES = main.c \
-	  memory.c
+SOURCES = src/main.c \
+	  src/memory.c
 
 ifeq ($(PLATFORM), MSP432)
-SOURCES += interrupts_msp432p401r_gcc.c
-SOURCES += startup_msp432p401r_gcc.c
-SOURCES += system_msp432p401r.c
+SOURCES += src/interrupts_msp432p401r_gcc.c
+SOURCES += src/startup_msp432p401r_gcc.c
+SOURCES += src/system_msp432p401r.c
 endif
 
 # Add your include paths to this variable
-INCLUDES = ../include/common
+INCLUDES = include/common
 
 ifeq ($(PLATFORM), MSP432)
-INCLUDES += ../include/msp432
-INCLUDES += ../include/CMSIS
+INCLUDES += include/msp432
+INCLUDES += include/CMSIS
 endif
 
